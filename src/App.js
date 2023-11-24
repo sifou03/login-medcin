f315a914ce4b88b68ed2a94ec2f0e97cdb8eb596
sifou03 <151889009+sifou03@users.noreply.github.com>
2023-11-24 12:32:20 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,64 @@
+
+
+
+import React from "react";
+
+
+function App() {
+  return (
+  
+    <>
+   <>
+  <div className="information">
+    <h1 className="titre">hey doctors! <br></br>connect and join us!!</h1>
+    <label htmlFor="nom" id="name">
+      name
+    </label>
+    <input type="text" placeholder="enter your name" />
+    <label htmlFor="prenom" id="prenom">
+      last name
+    </label>
+    <input type="text" placeholder="enter your  last  name" />
+    <label htmlFor="email" id="email">
+      email
+    </label>
+    <input type="text" placeholder="enter your Email" />
+    {" "}
+    
+    <label htmlFor="mot de passe">password</label>
+    <input type="password" placeholder="enter your password" />
+    <div className="rrr">
+    
+    
+         <input type="submit" Value="log in" />
+       
+
+  
+    </div>
+    
+  </div>
+  <div className="ddd">
+    <img
+      src="https://scontent.faae1-1.fna.fbcdn.net/v/t1.15752-9/387339057_349287281090075_2546292725828920031_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGEPF9PAl6IJUeahsaCOBPLWYNRVdmxFsVZg1FV2bEWxe2wKjUKVrwSllOJ7BCIEbxxt3gAmQXBHSVjdHVSdY6D&_nc_ohc=xbDsl4WgjyIAX8jbDgz&_nc_ht=scontent.faae1-1.fna&oh=03_AdSgNgJ_BxoohFcwBz9YbIOJ0GmaTFntdcsDur_oggbH-w&oe=656F404A"
+      alt=""
+      width="400px"
+      height="570px"
+    />
+  </div>
+  
+  
+  
+</>
+
+  </>
+  
+   
+  );
+}
+
+
+export default App;
+
+
+
+
